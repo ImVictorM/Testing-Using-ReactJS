7792f7e30ba7bbb1cc5dbffcc4393a05a1ac681f
Victor Mendes <victorutdsim7@gmail.com>
2022-09-19 16:12:06 -0300

    requisito 4 [completo]: testes componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente App.js ', () => {
+  it('Contém titulo com o texto "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('Página possui a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.src).toBe(
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
