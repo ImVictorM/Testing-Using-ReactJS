aca597519ab9b6915aed11636472de60f4355c66
Victor Mendes <victorutdsim7@gmail.com>
2022-09-19 16:02:45 -0300

    requisito 3 [completo]: testa componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente App.js ', () => {
+  it('É exibido "No favorite pokemon found" quando não existe favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
